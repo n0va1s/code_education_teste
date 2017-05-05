f26e176877011af8aa43db9eca1b05cebeb44812
Joao Paulo Cirino Silva de Novais <joao.novais@serpro.gov.br>
2017-05-05 18:10:58 -0300

    CategoriaController mais testada

--- test/src/JP/Sistema/Controller/CategoriaControllerTest.php
+++ test/src/JP/Sistema/Controller/CategoriaControllerTest.php
@@ -8,8 +8,7 @@ class CategoriaControllerTest extends WebTestCase
 {
     private $client;
     private $crawler;
-    private $id;
-
+    
     public function createApplication()
     {
         $app = require __DIR__.'/../../../../app.php';
@@ -36,17 +35,41 @@ class CategoriaControllerTest extends WebTestCase
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "HTTP status code nao confere");
         $this->assertTrue($this->client->getResponse()->headers->contains('Content-Type', 'application/json'));
         $this->assertGreaterThan(0, $this->client->getResponse()->getContent());
-        $this->id = $this->client->getResponse()->getContent();
+        return $this->client->getResponse()->getContent();
     }
-/*
-    public function testAtualizarCategoriaJson()
+    /**
+     * @depends testInserirCategoriaJson
+     */
+    public function testAtualizarCategoriaJson(string $id)
     {
-        $this->client->request('PUT', '/categoria/api/atualizar/'.$this->id, array('seqCategoria'=>$this->id, 'nomCategoria'=>'Categoria Nova'));
+        $this->client->request('PUT', '/categoria/api/atualizar/'.$id, array('seqCategoria'=>$id, 'nomCategoria'=>'Categoria Nova'));
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "HTTP status code nao confere");
         $this->assertTrue($this->client->getResponse()->headers->contains('Content-Type', 'application/json'));
-        $this->assertGreaterThan($this->id, $this->client->getResponse()->getContent());
+        $this->assertGreaterThanOrEqual($id, $this->client->getResponse()->getContent());
     }
-*/
+    /**
+     * @depends testInserirCategoriaJson
+     */
+    public function testListarCategoriaIdJson(string $id)
+    {
+        $this->client->request('GET', '/categoria/api/listar/'.$id);
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "HTTP status code nao confere");
+        $this->assertTrue($this->client->getResponse()->headers->contains('Content-Type', 'application/json'));
+        $c = count(json_decode($this->client->getResponse()->getContent(), true));
+        $this->assertEquals(1, $c);
+        $this->assertArrayHasKey('id', json_decode($this->client->getResponse()->getContent(), true)[0]);
+    }
+    /**
+     * @depends testInserirCategoriaJson
+     */
+    public function testApagarCategoriaJson(string $id)
+    {
+        $this->client->request('DELETE', '/categoria/api/apagar/'.$id, array('seqCategoria'=>$id));
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "HTTP status code nao confere");
+        $this->assertTrue($this->client->getResponse()->headers->contains('Content-Type', 'application/json'));
+        $this->assertTrue((boolean)$this->client->getResponse()->getContent());
+    }
+
     public function testListarCategoriaJson()
     {
         $this->client->request('GET', '/categoria/api/listar/json');
@@ -56,28 +79,9 @@ class CategoriaControllerTest extends WebTestCase
         $this->assertGreaterThan(0, $c);
         $this->assertArrayHasKey('id', json_decode($this->client->getResponse()->getContent(), true)[$c-1]);
     }
-/*
-    public function testListarCategoriaIdJson()
-    {
-        $this->client->request('GET', '/categoria/api/listar/'$this-id);
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "HTTP status code nao confere");
-        $this->assertTrue($this->client->getResponse()->headers->contains('Content-Type', 'application/json'));
-        $c = count(json_decode($this->client->getResponse()->getContent(), true));
-        $this->assertEquals(1, $c);
-        $this->assertArrayHasKey('id', json_decode($this->client->getResponse()->getContent(), true)[0]);
-    }
 
-    public function testApagarCategoriaJson()
-    {
-        $this->client->request('DELETE', '/categoria/api/apagar/'.$this->id, array('seqCategoria'=>$this->id));
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "HTTP status code nao confere");
-        $this->assertTrue($this->client->getResponse()->headers->contains('Content-Type', 'application/json'));
-        $this->assertTrue($this->client->getResponse()->getContent());
-    }
-*/
     public function tearDown()
     {
         unset($this->client);
-        unset($this->id);
     }
 }
